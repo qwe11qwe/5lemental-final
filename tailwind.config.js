e9bebc4873cd2ab3b3bc8decb8d81b1502ecf252
dani-day <allkite5122@gmail.com>
2023-09-08 14:53:48 +0900

    🎉 초기 Setting (tailwind.config.js 색상 변경 및 css 연결)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.jsx'],
   theme: {
-  extend: {
-    fontFamily: {
-      nanum: ['NanumSquare'],
-      dohyeon: ['BMDOHYEON'],
-    },
-    colors: {
-      '--fridge-primary': '#0E3348',
-      '--fridge-secondary': '#0E3348',
-      '--fridge-gray': '#D9D9D9',
-      '--fridge-input-gray': '#B0B0B0',
-      '--fridge-nav-gray': '#666565',
-      '--fridge-black': '#000000',
-      '--fridge-white': '#FFFFFF',
-      '--fridge-skyblue': '#DCF2FF',
-      '--fridge-red': '#F83319',
+    extend: {
+      fontFamily: {
+        nanum: ['NanumSquare'],
+        dohyeon: ['BMDOHYEON'],
+      },
+      colors: {
+        '--fridge-primary': '#0E3348',
+        '--fridge-secondary': '#EBEBEB',
+        '--fridge-gray': '#D9D9D9',
+        '--fridge-input-gray': '#B0B0B0',
+        '--fridge-nav-gray': '#666565',
+        '--fridge-black': '#000423',
+        '--fridge-white': '#FFFFFF',
+        '--fridge-skyblue': '#DCF2FF',
+        '--fridge-red': '#F83319',
+      },
     },
   },
-},
   plugins: [],
 };

7af9bf52cd5401aad52a2abf186961aa6cbae85a
h2s0 <h2s0_@naver.com>
2023-09-14 00:51:45 +0900

    🎨가운데 정렬 및 기능 코드 수정

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -29,49 +29,50 @@ console.log(search)
 console.log(inputRef.current.value)
 
 const toggleInputSearch = () => {
-  if ( inputRef.current.value) {
-    setSearchResult(search.filter((cook) => search.includes(inputRef.current.value)));
+  if (inputRef.current.value) {
+    setSearchResult(search.filter(() => search.includes(inputRef.current.value)));
   } else {
-    setSearchResult([]);
+    setSearchResult('');
   }
 }
 
-console.log(searchResult)
-
 function searchMenu (e) {
   e.preventDefault();
-  console.log('click');
   toggleInputSearch();
 }
 
-// 규민 : 리액트 쿼리를 써서 캐싱을 하는게 좋지 않을까.. 캐싱 안하면 검색이 바뀔때마다 다시 렌더링 된다. 데이터를 한 번만 불러와서 렌더링하지 않고 사용자가 필요할 때마다 꺼내 쓸 수 있다. useRef 를 사용해 
+// 규민 : 리액트 쿼리를 써서 캐싱을 하는게 좋지 않을까.. 캐싱 안하면 검색이 바뀔때마다 다시 렌더링 된다. 데이터를 한 번만 불러와서 렌더링하지 않고 사용자가 필요할 때마다 꺼내 쓸 수 있다. useRef 를 사용해
+
 
   return(
     <>
-    <div className='flex mt-5 justify-center'>
-      <input
-        type="text"
-        placeholder="메뉴를 검색해주세요."
-        className="w-4/5 h-7 pl-1 placeholder:-text--fridge-input-gray font-nanum border-b-2 -border--fridge-gray focus:outline-none"
-        ref={inputRef}
-      />
-      <button
-        type="button"
-        className='w-5 h-5 bg-search-icon my-auto ml-2'
-        onClick={searchMenu}
-        >
-      </button>
-    </div>
-    <div className='mt-4'>
+    <div className='w-full max-w-[820px] m-auto'>
+      <div className='flex mt-8 ml-[20px] relative'>
+        <input
+          type='text'
+          placeholder='메뉴를 검색해주세요.'
+          className="w-11/12 h-7 pl-1 placeholder:-text--fridge-input-gray font-nanum border-b-2 -border--fridge-gray focus:outline-none"
+          ref={inputRef}
+        />
+        <button
+          type='button'
+          className='w-5 h-5 bg-search-icon my-auto ml-2 absolute right-[9%]'
+          onClick={searchMenu}
+          >
+        </button>
+      </div>
+      <div className='mt-4'>
       {searchResult.toSorted().map(
         (cook, index) => 
           (
-            <li key={index} className='ml-5 text-xl'>
-              <span>{cook}</span>
+            <li key={index} className='ml-5 text-sm'>
+              <span>{searchResult}</span>
             </li>
           )
       )}
+      </div>
     </div>
+    
     </>
   )
 }

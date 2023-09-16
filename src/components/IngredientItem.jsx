18aaaf56ce8cc42282497c40f543dda603942c1b
h2s0 <h2s0_@naver.com>
2023-09-17 00:15:16 +0900

    🎨 UI 코드 작성

--- src/components/IngredientItem.jsx
+++ src/components/IngredientItem.jsx
@@ -1,5 +1,12 @@
-function ingredientItem() {
-
+function IngredientItem() {
+  return(
+    <>
+    <div className="w-[55px] h-[74px] bg-pink-100">
+      <div className="w-[55px] h-[55px]"></div>
+      <div className="font-nanum w-auto h-4 -bg--fridge-bg-gray rounded-md text-center text-xs overflow-hidden"></div>
+    </div>
+    </>
+  )
 }
 
-export default ingredientItem
+export default IngredientItem

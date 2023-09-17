1cf6b65bcd1122f64771f4a3d8f88d7cfa5a051a
h2s0 <h2s0_@naver.com>
2023-09-17 22:36:33 +0900

    ✨재료 전체 정보 불러오고 렌더링하는 코드 작성

--- src/components/IngredientItem.jsx
+++ src/components/IngredientItem.jsx
@@ -1,9 +1,43 @@
+import { getPbImageURL } from "@/utils/getPbImageURL"
+import pb from '@/api/pocketbase';
+import { useState, useEffect } from 'react'
+
 function IngredientItem() {
+
+  // 재료 전체 정보
+  const [ingredientInfo, setingredientInfo] = useState([]);
+
+  useEffect(() => {
+    async function fetchInfoList() {
+      try {
+        const infoList = await pb.collection('ingredients').getFullList();
+        setingredientInfo(infoList)
+        console.log(ingredientInfo)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+    fetchInfoList();
+  }, []);
+
   return(
     <>
-    <div className="w-[55px] h-[74px] bg-pink-100">
-      <div className="w-[55px] h-[55px]"></div>
-      <div className="font-nanum w-auto h-4 -bg--fridge-bg-gray rounded-md text-center text-xs overflow-hidden"></div>
+    <div className="flex gap-1 flex-wrap justify-center">
+      {ingredientInfo.map((ingredient) => (
+        <div
+          key={ingredient.id}
+          className="w-[55px] h-[74px]">
+            <img
+              src={getPbImageURL(ingredient, 'photo')}
+              alt={ingredient.name}
+            />
+          <div
+            className="font-nanum w-auto h-4 mt-1 -bg--fridge-bg-gray rounded-md text-center text-xs overflow-hidden"
+          >
+            {ingredient.name}
+          </div>
+        </div>
+      ))}
     </div>
     </>
   )

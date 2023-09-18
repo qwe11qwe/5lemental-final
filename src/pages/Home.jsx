fc66399d62b4dcb2a3988c66f9e18c4b1e186c05
h2s0 <h2s0_@naver.com>
2023-09-18 22:12:02 +0900

    ✨ Home 페이지 재료, 버튼 컴포넌트 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import pb from '@/api/pocketbase';
+import IngredientButtonSero from '@/components/IngredientButtonSero';
+import Button from '@/components/button/Button'
 
 function Home() {
   console.log(pb.authStore.token);
+
+  return(
+    <>
+    <IngredientButtonSero></IngredientButtonSero>
+    <Button type='button' navigateTo='/fridgemenu'>
+      내 냉장고 속 재료로 요리하기
+    </Button>
+    </>
+  )
 }
 
 export default Home;

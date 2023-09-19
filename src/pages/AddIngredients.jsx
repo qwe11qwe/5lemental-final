69e679368be0dc84792adc031987cdf9de8fdd5b
dani-day <allkite5122@gmail.com>
2023-09-19 17:04:25 +0900

    🚑 버그 해결을 위한 폴더 및 파일명 변경

--- src/pages/AddIngredients.jsx
+++ src/pages/AddIngredients.jsx
@@ -1,9 +1,9 @@
 import SearchInput from '@/components/SearchInput';
-import IngredientButtonGaro from './../components/ingredient-button-garo';
+import IngredientButtonGaro from '../components/IngredientButtonGaro';
 import { useEffect, useState } from 'react';
 import debounce from 'lodash.debounce';
 
-function AddIngredients () {
+function AddIngredients() {
   const [inputValue, setInputValue] = useState('');
 
   // 이미지를 클릭했을 때 호출될 함수
@@ -29,31 +29,32 @@ function AddIngredients () {
       <div className="w-screen h-14 bg-red-500"></div>
       {/* <SearchInput searchType={'cook'}></SearchInput> */}
       <form className="w-screen h-14 bg-orange-400 flex justify-center items-center">
-        <input 
-          type="text" 
-          className='border-0 border-b-2 border-solid border-gray-500 h-6 w-3/4'
+        <input
+          type="text"
+          className="border-0 border-b-2 border-solid border-gray-500 h-6 w-3/4"
           value={inputValue}
           onChange={handleInputChange} // 디바운싱된 핸들러로 변경
-          />
-          {/* 디바운싱 처리해서 제일 마지막에 상태 변경되도록 하기 or 상태 변경은 버튼 누르면 되도록 하고 제일 마지막에 어디 저장소로 이동되게 하기?*/}
+        />
+        {/* 디바운싱 처리해서 제일 마지막에 상태 변경되도록 하기 or 상태 변경은 버튼 누르면 되도록 하고 제일 마지막에 어디 저장소로 이동되게 하기?*/}
         <button>
-          <img 
-            src="./../assets/icons/search.svg" 
-            alt="" 
-            className='w-6 h-6' 
+          <img
+            src="./../assets/icons/search.svg"
+            alt=""
+            className="w-6 h-6"
             onClick={() => handleImageClick(inputValue)} // 클릭 시 현재 입력값
           />
         </button>
-        
       </form>
       <div className="w-screen h-3/5 overflow-y-scroll">
-        <IngredientButtonGaro ingredientName={inputValue}></IngredientButtonGaro>
+        <IngredientButtonGaro
+          ingredientName={inputValue}
+        ></IngredientButtonGaro>
       </div>
       <div className="w-4/5 h-14 mx-auto bg-green-400 rounded-lg"></div>
-      
+
       {/* <NavBar></NavBar> */}
     </div>
-  )
+  );
 }
 
-export default AddIngredients
+export default AddIngredients;

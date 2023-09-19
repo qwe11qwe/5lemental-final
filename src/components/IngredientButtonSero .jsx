196fc92e2f0c2a24620aa59c2ab802cb814b1ec5
h2s0 <h2s0_@naver.com>
2023-09-19 10:30:37 +0900

    ✨ 재료 세로 칸 UI, 전체 리스트 렌더링

---
+++ src/components/IngredientButtonSero .jsx
@@ -0,0 +1,43 @@
+import pb from '@/api/pocketbase';
+import { useState, useEffect } from "react";
+import { getPbImageURL } from "@/utils/getPbImageURL"
+
+
+function IngredientButtonSero () {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchList() {
+      try {
+        const list = await pb.collection('ingredients').getFullList();
+        setData(list)
+        console.log(list);
+      }
+      catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+    fetchList();
+  }, []);
+
+  return(
+    <>
+      {data.map((item) => (
+        <div
+          className="w-[78px] h-[95px] -bg--fridge-secondary border-none rounded-md flex flex-col justify-center self-center pt-[6px]"
+          key={item.id}>
+          <div className="w-[62px] h-[62px] bg-white items-center">
+            <img
+              src={getPbImageURL(item,'photo')}
+              alt={item.name}
+              className=''
+            />
+          </div>
+        <div className="font-dohyeon text-center">{item.name}</div>
+      </div>
+      ))}
+    </>
+  )
+}
+
+export default IngredientButtonSero

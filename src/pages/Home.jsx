f85d04339a601cd4236f4e0b3a7bce9b6cbdf718
h2s0 <h2s0_@naver.com>
2023-09-20 17:08:50 +0900

    ✨ Home 페이지 UI 추가 ( 진행 중 )

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import pb from '@/api/pocketbase';
 import IngredientButtonSero from '@/components/IngredientButtonSero ';
+import SearchInput from '@/components/SearchInput';
 import Button from '@/components/button/Button';
 
 function Home() {
 
   return(
     <>
+    <SearchInput></SearchInput>
+      <h1 className='font-dohyeon'>내 재료</h1>
       <IngredientButtonSero></IngredientButtonSero>
       <Button type='button'>
         내 냉장고 속 재료로 요리하기

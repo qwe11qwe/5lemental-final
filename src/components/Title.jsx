81c32d00d925c29f7099d3df19a3656ad4cb0e23
h2s0 <h2s0_@naver.com>
2023-09-21 23:20:39 +0900

    ✨ Title 컴포넌트 완성 - 글씨체, 작성 내용 매개변수로 받아 사용

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,5 +1,33 @@
-function Title() {
+function Title({ size, contents }) {
+  let fontSize = '';
 
+  switch(size) {
+    // 12px
+    case 'xs' : fontSize = 'text-xs';
+    break;
+    // 14px
+    case 'sm' : fontSize = 'text-sm';
+    break;
+    // 16px
+    case 'base' : fontSize = 'text-base';
+    break;
+    // 18px
+    case 'lg' : fontSize = 'text-lg';
+    break;
+    // 20px
+    case 'xl' : fontSize = 'text-xl';
+    break;
+    // 24px
+    case '2xl' : fontSize = 'text-2xl';
+    break;
+    // 30px
+    case '3xl' : fontSize = 'text-3xl';
+    break;
+  }
+
+  return (
+		<span className={`font-dohyeon ${fontSize}`}>{contents}</span>
+	);
 }
 
 export default Title;

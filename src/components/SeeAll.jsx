23bd044ccc4979f66860ab5f1e2c650a3c783a37
h2s0 <h2s0_@naver.com>
2023-09-22 00:08:59 +0900

    ✨ SeeAll 컴포넌트 ( 전체보기 ) 완성

--- src/components/SeeAll.jsx
+++ src/components/SeeAll.jsx
@@ -1,5 +1,13 @@
-function SeeAll() {
+import { Link } from "react-router-dom"
 
+function SeeAll() {
+  return(
+    <button className="text-xs font-nanum">
+      <Link to='/menulist'>
+        전체보기
+      </Link>
+    </button>
+  )
 }
 
 export default SeeAll

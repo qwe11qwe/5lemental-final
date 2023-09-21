ae0ebbf80075a95b5bf59f97325ada30d7ebbe95
qwe11qwe <rlatjdwo811@gmail.com>
2023-09-22 03:04:14 +0900

    💎 RecipeLiked 파일 에서 메뉴 리스트 렌더링 완료, 좋아요 버튼 구현중

--- src/components/likeButton/Likebutton.jsx
+++ src/components/likeButton/Likebutton.jsx
@@ -1,104 +1,28 @@
-import pocketbase from '@/api/pocketbase';
-import { getPocketHostImageURL } from '@/utils';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import BookMark from '../BookMark';
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { heartLine, heartFill } from '@/assets/icons/svg-icons.js';
+// import S from './LikeButton.module.css';
+// import pb from '@/api/pocketbase';
 
-// 데이터 요청 함수 (query function)
-const getRecommends = async (userId) => {
-  return await pocketbase.collection('recommends').getFullList({
-    filter: `(userEmail?~'${userId}')`,
-    fields: 'collectionId,id,image',
-  });
-};
+function Likebutton() {
+  const [isLiked, setIsLiked] = useState(false);
+  const location = useLocation();
 
-// 데이터의 userEmail 필드에서 삭제 요청 함수 (mutation function)
-const removeRecommend = async ({ recommendId, userId }) => {
-  return await pocketbase.collection('recommends').update(recommendId, {
-    'userEmail-': userId,
-  });
-};
-
-// 로그인 사용자 (더미 데이터)
-// 실제 로그인 후 `pocketbase.authStore.model`에서 정보를 가져올 수 있습니다.
-const dummyLoginUserInfo = {
-  id: 'ypejq0ceyg9dpza',
-  username: 'hyeonjuu',
-  email: 'janghyeonjuu@gmail.com',
-};
-
-export default function BookmarkList() {
-  // 로그인 사용자 정보
-  const user = pocketbase.authStore.model ?? dummyLoginUserInfo;
-
-  // 쿼리 클라이언트 인스턴스 가져오기
-  const queryClient = useQueryClient();
-
-  // 쿼리 키
-  const queryKey = ['recommends', user.id];
-
-  // React Query를 사용한 데이터 쿼리(query) 요청
-  const { isLoading, error, data } = useQuery({
-    queryKey: queryKey,
-    queryFn: () => getRecommends(user.id),
-    refetchOnReconnect: false,
-    refetchOnWindowFocus: false,
-  });
-
-  // React Query를 사용한 데이터 수정(mutation) 요청
-  const mutation = useMutation({
-    mutationFn: removeRecommend,
-    onMutate: async ({ recommendId }) => {
-      await queryClient.cancelQueries({ queryKey: queryKey });
-
-      const previousList = queryClient.getQueryData(queryKey);
-
-      queryClient.setQueryData(queryKey, (list) => {
-        return list.filter((item) => item.id !== recommendId);
-      });
-
-      return { previousList };
-    },
-    onSuccess: async () => {
-      queryClient.invalidateQueries({ queryKey: queryKey });
-    },
-    onError: (error, removedBookmark, context) => {
-      queryClient.setQueryData(queryKey, context.previousList);
-    },
-  });
-
-  const handleRemoveBookmark = (recommendId, userId) => async () => {
-    mutation.mutate({
-      recommendId,
-      userId,
-    });
-  };
-
-  if (isLoading) {
-    return <div>로딩 중...</div>;
-  }
-
-  if (error) {
-    return <div role="alert">{error.toString()}</div>;
-  }
+  useEffect(() => {
+    if (location.pathname === '/home') {
+      setIsLiked(true);
+    } else {
+      setIsLiked(false);
+    }
+  }, [location]);
 
   return (
-    <ul className="mx-auto grid gap-4 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
-      {data?.map?.((item) => (
-        <li key={item.id} className="relative min-w-[360px]">
-          <button
-            type="button"
-            className="absolute right-4 top-4 cursor-pointer"
-            onClick={handleRemoveBookmark(item.id, user.id)}
-          >
-            <BookMark color="#C9ECFF" />
-          </button>
-          <img
-            src={getPocketHostImageURL(item).split(',')[0]}
-            alt=""
-            className="box-content aspect-square  rounded-lg border-[1px] border-gray-2 object-cover md:h-[380px] lg:h-[420px] xl:h-[400px]"
-          />
-        </li>
-      ))}
-    </ul>
+    <div>
+      <button type="button" onClick={() => setIsLiked(!isLiked)}>
+        <img src={isLiked ? heartLine : heartFill} />
+      </button>
+    </div>
   );
 }
+
+export default Likebutton;
--- src/pages/RecipeLiked.jsx
+++ src/pages/RecipeLiked.jsx
@@ -1,3 +1,22 @@
-function RecipeLiked() {}
+import MenuImge from '@/components/MenuImge';
+import { useState, useRef } from 'react';
+import Likebutton from '@/components/likeButton/Likebutton';
+
+function RecipeLiked() {
+  const Add = 'Add';
+  const inputRef = useRef(null);
+  console.log(inputRef);
+  const [inputValue] = useState('');
+
+  return (
+    <div className="h-screen max-w-[820px] m-auto">
+      <div className="flex mt-8 ml-[20px] "></div>
+      <div className="w-screen  overflow-y-scroll max-w-[820px] m-auto min-w-[320px]">
+        <MenuImge MenuImgeName={inputValue} print={Add}></MenuImge>
+        <Likebutton />
+      </div>
+    </div>
+  );
+}
 
 export default RecipeLiked;

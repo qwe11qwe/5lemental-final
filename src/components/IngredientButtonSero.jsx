6d7758461b259271dafcb8af3be067f3689114c6
h2s0 <h2s0_@naver.com>
2023-09-21 15:35:02 +0900

    ✨내 재료 스와이퍼 추가

--- src/components/IngredientButtonSero.jsx
+++ src/components/IngredientButtonSero.jsx
@@ -2,11 +2,16 @@ import pb from '@/api/pocketbase';
 import { useState, useEffect } from "react";
 import { getPbImageURL } from "@/utils/getPbImageURL"
 import useAuthStore from '@/store/auth';
+// 스와이퍼
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from 'swiper/modules';
 
 function IngredientButtonSero () {
   // 내 재료 정보
   const [myIngredient, setMyIngredient] = useState([]);
-
+  // 유저 정보
   const { user } = useAuthStore();
   console.log(user);
 
@@ -32,9 +37,15 @@ function IngredientButtonSero () {
 
   return(
     <div className='w-full max-w-[820px] m-auto'>
-      <div className='flex ml-5 gap-2'>
+      <Swiper
+        slidesPerView='auto'
+        spaceBetween={8}
+        freeMode={true}
+        modules={[FreeMode]}
+        className="mx-5 py-3"
+      >
         {myIngredient.map((item) => (
-          <div
+          <SwiperSlide
             className="w-[78px] h-[95px] -bg--fridge-bg-gray border-none rounded-md flex flex-col justify-center self-center"
             key={item.id}>
             <div className="w-[62px] h-[62px] items-center mx-2">
@@ -47,9 +58,9 @@ function IngredientButtonSero () {
           <span className="font-dohyeon text-[12px] text-center mt-[6px]">
             {item.name}
           </span>
-        </div>
+        </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </div>
   )
 }
